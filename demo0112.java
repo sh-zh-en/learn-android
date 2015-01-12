f25c0e6b9d4d564e1b5266d8712fb8c3c54bd8e4
Haiming.Mao <sunrain_wind@163.com>
2015-01-13 02:32:15 +0800

    add demo0112.java

---
+++ demo0112.java
@@ -0,0 +1,27 @@
+class Demo0112{
+	public static void main(String[] args){
+		System.out.println("hello world");
+		int a=10;
+		int b=20;
+		System.out.println("交换前a="+a+",b="+b);
+		int max1=a>b?a:b;
+		System.out.println("The max = "+max1);
+		//实现a与b值的交换
+		/*
+		a=a^b;
+		b=a^b;
+		a=a^b;
+		System.out.println("交换后a="+a+",b="+b);
+		*/
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("交换后a="+a+",b="+b);
+		int max;
+		max=a>b?a:b;
+		System.out.println("The max = "+max);
+
+
+		}
+	}
+
