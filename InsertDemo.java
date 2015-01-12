3cf28378b3f1753e384a1611803db7ea04bd9691
Haiming.Mao <sunrain_wind@163.com>
2015-01-13 02:33:09 +0800

    add InsertDemo.java

---
+++ InsertDemo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class InsertDemo{
+	public static void main(String[] args){
+	System.out.println("Input:");
+	Scanner sc=new Scanner(System.in);
+	int x=sc.nextInt();
+    	System.out.println("The Scanner you inputted is "+x);
+	
+	}
+
+}
